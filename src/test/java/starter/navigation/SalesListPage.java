d1280787301b912276aa6f9d903ea6bc8571c787
Hevindu <95610343+Hevb890@users.noreply.github.com>
2026-06-09 14:27:22 +0530

    ADMIN_UI_TESTING_SALES

---
+++ src/test/java/starter/navigation/SalesListPage.java
@@ -0,0 +1,50 @@
+package starter.navigation;
+
+import net.serenitybdd.core.annotations.findby.FindBy;
+import net.serenitybdd.core.pages.PageObject;
+import net.serenitybdd.core.pages.WebElementFacade;
+import net.serenitybdd.annotations.DefaultUrl;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+@DefaultUrl("http://localhost:8080/ui/sales")
+public class SalesListPage extends PageObject {
+
+    @FindBy(xpath = "//button[contains(text(),'Sell Plant')] | //a[contains(text(),'Sell Plant')]")
+    private WebElementFacade sellPlantButton;
+
+    @FindBy(xpath = "(//button[descendant::i[contains(@class, 'bi-trash')]])[1]")
+    private WebElementFacade firstDeleteButton;
+
+    public boolean isSellPlantButtonVisible() {
+        return sellPlantButton.isCurrentlyVisible();
+    }
+
+    public void clickSellPlantButton() {
+        sellPlantButton.waitUntilClickable().click();
+    }
+
+    public void clickFirstDeleteButton() {
+        firstDeleteButton.waitUntilClickable().click();
+    }
+
+    // Check Prompt Visibility
+    public boolean isConfirmationPromptVisible() {
+        try {
+            WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(5));
+            Alert alert = wait.until(ExpectedConditions.alertIsPresent());
+            alert.dismiss();
+            return true;
+        } catch (NoAlertPresentException | org.openqa.selenium.TimeoutException e) {
+            return false;
+        }
+    }
+
+    public boolean isOnSalesListPage() {
+        return getDriver().getCurrentUrl().contains("/sales");
+    }
+}
---
+++ src/test/java/starter/navigation/SellPlantPage.java
@@ -0,0 +1,80 @@
+package starter.navigation;
+
+import net.serenitybdd.core.annotations.findby.FindBy;
+import net.serenitybdd.core.pages.PageObject;
+import net.serenitybdd.core.pages.WebElementFacade;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.List;
+
+public class SellPlantPage extends PageObject {
+
+    @FindBy(css = "select[name='plantId'], select[id='plantId']")
+    private WebElementFacade plantDropdown;
+
+    @FindBy(css = "input[name='quantity'], input[id='quantity'], input[type='number']")
+    private WebElementFacade quantityField;
+
+    @FindBy(xpath = "//button[@type='submit'] | //button[normalize-space()='Sell'] | //button[normalize-space()='Save']")
+    private WebElementFacade submitButton;
+
+    @FindBy(xpath = "//button[normalize-space()='Cancel'] | //a[normalize-space()='Cancel']")
+    private WebElementFacade cancelButton;
+
+    @FindBy(css = ".error, .alert-danger, [class*='error-msg'], [class*='stock-error']")
+    private WebElementFacade errorMessage;
+
+    public void openPlantDropdown() {
+        plantDropdown.waitUntilVisible().click();
+    }
+
+    public boolean plantDropdownHasOptions() {
+        List<WebElement> options = getDriver().findElements(
+                By.cssSelector(
+                        "select[id='plantId'] option, select[name='plantId'] option"));
+        return options.stream()
+                .filter(o -> !o.getAttribute("value").isEmpty()
+                        && !o.getText().toLowerCase().contains("select"))
+                .count() > 0;
+    }
+
+    public boolean dropdownOptionsContainStockInfo() {
+        List<WebElement> options = getDriver().findElements(
+                By.cssSelector(
+                        "select[name='plant'] option, select[id='plant'] option, select[name='plantId'] option"));
+        return options.stream()
+                .filter(o -> !o.getAttribute("value").isEmpty())
+                .anyMatch(o -> o.getText().matches(".*\\d+.*"));
+    }
+
+    public void selectFirstAvailablePlant() {
+        plantDropdown.waitUntilVisible();
+        Select select = new Select(plantDropdown.getElement());
+        for (WebElement option : select.getOptions()) {
+            if (!option.getAttribute("value").isEmpty()
+                    && !option.getText().toLowerCase().contains("select")) {
+                select.selectByVisibleText(option.getText());
+                break;
+            }
+        }
+    }
+
+    public void enterQuantity(String quantity) {
+        quantityField.waitUntilVisible().clear();
+        quantityField.type(quantity);
+    }
+
+    public void clickSubmit() {
+        submitButton.waitUntilClickable().click();
+    }
+
+    public void clickCancel() {
+        cancelButton.waitUntilClickable().click();
+    }
+
+    public boolean isErrorMessageDisplayed() {
+        return errorMessage.isCurrentlyVisible();
+    }
+}
